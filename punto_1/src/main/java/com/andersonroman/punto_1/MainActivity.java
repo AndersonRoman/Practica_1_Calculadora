6091658c0f14eb0c73f9d0fcb53f90fe7f481f3a
AndersonRoman <ander-353@hotmail.com>
2017-02-27 15:53:30 -0500

    Punto 1, Figuras geometricas

--- punto_1/src/main/java/com/andersonroman/punto_1/MainActivity.java
+++ punto_1/src/main/java/com/andersonroman/punto_1/MainActivity.java
@@ -2,12 +2,154 @@ package com.andersonroman.punto_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import static com.andersonroman.punto_1.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    RadioButton rCuadrado, rCirculo, rTriangulo,rCubo;
+    EditText eLado,eRadio,eBase,eAltura,ePerimetro,eArea,eVolumen;
+    Button bCalcular;
+    double Lado, Radio, Base, Altura, Perimetro, Area, Volumen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rCuadrado= (RadioButton) findViewById(R.id.rCuadrado);
+        rCirculo= (RadioButton) findViewById(R.id.rCirculo);
+        rTriangulo=(RadioButton) findViewById(R.id.rTriangulo);
+        rCubo=(RadioButton) findViewById(R.id.rCubo);
+
+        eLado=(EditText) findViewById(R.id.eLado);
+        eRadio=(EditText) findViewById(R.id.eRadio);
+        eBase=(EditText) findViewById(R.id.eBase);
+        eAltura=(EditText) findViewById(R.id.eAltura);
+        ePerimetro=(EditText) findViewById(R.id.ePerimetro);
+        eArea=(EditText) findViewById(R.id.eArea);
+        eVolumen=(EditText) findViewById(R.id.eVolumen);
+
+        bCalcular=(Button) findViewById(R.id.bCalcular);
+
+        eLado.setVisibility(android.view.View.INVISIBLE);
+        eRadio.setVisibility(android.view.View.INVISIBLE);
+        eBase.setVisibility(android.view.View.INVISIBLE);
+        eAltura.setVisibility(android.view.View.INVISIBLE);
+        eVolumen.setVisibility(android.view.View.INVISIBLE);
+    }
+
+    public void click(android.view.View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.rCuadrado:
+                if (checked){
+                    rCirculo.setChecked(false);
+                    rTriangulo.setChecked(false);
+                    rCubo.setChecked(false);
+                    eLado.setVisibility(android.view.View.VISIBLE);
+                    eRadio.setVisibility(android.view.View.INVISIBLE);
+                    eBase.setVisibility(android.view.View.INVISIBLE);
+                    eAltura.setVisibility(android.view.View.INVISIBLE);
+                    eVolumen.setVisibility(android.view.View.INVISIBLE);
+
+                    bCalcular.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (!eLado.getText().toString().isEmpty()) {
+                                Lado = Double.parseDouble(eLado.getText().toString());
+                                Perimetro = 4 * Lado;
+                                Area = Lado * Lado;
+                                ePerimetro.setText(String.valueOf(Perimetro));
+                                eArea.setText(String.valueOf(Area));
+                            }
+                        }
+                    });
+
+                }
+                break;
+            case R.id.rCirculo:
+                if (checked){
+                    rCuadrado.setChecked(false);
+                    rTriangulo.setChecked(false);
+                    rCubo.setChecked(false);
+                    eRadio.setVisibility(android.view.View.VISIBLE);
+                    eLado.setVisibility(android.view.View.INVISIBLE);
+                    eBase.setVisibility(android.view.View.INVISIBLE);
+                    eAltura.setVisibility(android.view.View.INVISIBLE);
+                    eVolumen.setVisibility(android.view.View.INVISIBLE);
+
+                    bCalcular.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (!eRadio.getText().toString().isEmpty()){
+                                Radio=Double.parseDouble(eRadio.getText().toString());
+                                Perimetro=2*Math.PI*Radio;
+                                Area=Math.PI*Radio*Radio;
+                                ePerimetro.setText(String.valueOf(Perimetro));
+                                eArea.setText(String.valueOf(Area));
+                            }
+                        }
+                    });
+                }
+                break;
+            case R.id.rTriangulo:
+                if (checked){
+                    rCirculo.setChecked(false);
+                    rCuadrado.setChecked(false);
+                    rCubo.setChecked(false);
+                    eBase.setVisibility(android.view.View.VISIBLE);
+                    eAltura.setVisibility(android.view.View.VISIBLE);
+                    eLado.setVisibility(android.view.View.INVISIBLE);
+                    eRadio.setVisibility(android.view.View.INVISIBLE);
+                    eVolumen.setVisibility(android.view.View.INVISIBLE);
+
+                    bCalcular.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if(!eBase.getText().toString().isEmpty() && !eAltura.getText().toString().isEmpty()){
+                                Base=Double.parseDouble(eBase.getText().toString());
+                                Altura=Double.parseDouble(eAltura.getText().toString());
+                                Perimetro=3*Base;
+                                Area=Base*Altura/2;
+                                ePerimetro.setText(String.valueOf(Perimetro));
+                                eArea.setText(String.valueOf(Area));
+                            }
+                        }
+                    });
+
+                }
+                break;
+            case R.id.rCubo:
+                if (checked){
+                    rCirculo.setChecked(false);
+                    rTriangulo.setChecked(false);
+                    rCuadrado.setChecked(false);
+                    eLado.setVisibility(android.view.View.VISIBLE);
+                    eVolumen.setVisibility(android.view.View.VISIBLE);
+                    eRadio.setVisibility(android.view.View.INVISIBLE);
+                    eBase.setVisibility(android.view.View.INVISIBLE);
+                    eAltura.setVisibility(android.view.View.INVISIBLE);
+                    ePerimetro.setVisibility(android.view.View.INVISIBLE);
+                    eArea.setVisibility(android.view.View.INVISIBLE);
+
+                    bCalcular.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (!eLado.getText().toString().isEmpty()) {
+                                Lado = Double.parseDouble(eLado.getText().toString());
+                                Volumen = Lado * Lado * Lado;
+                                eVolumen.setText(String.valueOf(Volumen));
+                            }
+                        }
+                    });
+                }
+                break;
+        }
     }
 }
---
+++ punto_2/src/main/java/com/andersonroman/punto_2/MainActivity.java
@@ -0,0 +1,67 @@
+package com.andersonroman.punto_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText ePracticas, eAvanceI, eAvanceII,eAvanceIII, eFinal;
+    Button bCalcular;
+    double practicas,avanceI,avanceII,avanceIII,pfinal,notaFinal;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ePracticas=(EditText) findViewById(R.id.ePracticas);
+        eAvanceI= (EditText) findViewById(R.id.eAvanceI);
+        eAvanceII=(EditText) findViewById(R.id.eAvanceII);
+        eAvanceIII= (EditText) findViewById(R.id.eAvanceIII);
+        eFinal= (EditText) findViewById(R.id.eAvanceIII);
+
+        bCalcular= (Button) findViewById(R.id.bCalcular);
+
+    }
+
+    public void click(View view) {
+
+        bCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!(ePracticas.getText().toString().isEmpty()&& eAvanceI.getText().toString().isEmpty() && eAvanceII.getText().toString().isEmpty() && eAvanceIII.getText().toString().isEmpty() && eFinal.getText().toString().isEmpty() )){
+                    practicas=Double.parseDouble(ePracticas.getText().toString());
+                    avanceI=Double.parseDouble(eAvanceI.getText().toString());
+                    avanceII=Double.parseDouble(eAvanceII.getText().toString());
+                    avanceIII=Double.parseDouble(eAvanceIII.getText().toString());
+                    pfinal=Double.parseDouble(eFinal.getText().toString());
+
+                    notaFinal=practicas*.6+avanceI*.005+avanceII*.007+avanceIII*.008+pfinal*.2;
+
+                    if (notaFinal>=0 && notaFinal<=1){
+                        Toast.makeText(MainActivity.this,"Estas en el lugar equivocado",Toast.LENGTH_LONG).show();
+                    }
+                    if (notaFinal>1 && notaFinal<=2){
+                        Toast.makeText(MainActivity.this,"Remal",Toast.LENGTH_LONG).show();
+                    }
+                    if (notaFinal>2 && notaFinal<=3){
+                        Toast.makeText(MainActivity.this,"Es posible recuperarse",Toast.LENGTH_LONG).show();
+                    }
+                    if (notaFinal>3 && notaFinal<=4){
+                        Toast.makeText(MainActivity.this,"Normalito",Toast.LENGTH_LONG).show();
+                    }
+                    if (notaFinal>4 && notaFinal<=4.5){
+                        Toast.makeText(MainActivity.this,"Muy Bien",Toast.LENGTH_LONG).show();
+                    }
+                    if (notaFinal>4.5&& notaFinal<=5){
+                        Toast.makeText(MainActivity.this,"Excenlente estudiante",Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+        });
+    }
+}
